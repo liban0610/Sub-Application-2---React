2b28220d42f0ede7ab3bf8254a649db7f949b3bb
debug-ctrl <115629541+debug-ctrl@users.noreply.github.com>
2024-12-01 22:26:09 +0100

    logging og errorhandling

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+005559eea39c7a290ab1b3ffce77e5224e44e8ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6db26b8b8acb83a269e8edbfc72e74a203429376")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
